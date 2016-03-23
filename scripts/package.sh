3ade4c29515e992a4ef9748e687b3e44b0a2844c
Marcus Thiesen <mt@hubrick.com>
2016-03-23 19:28:49 +0100

    Better way to get the codename

--- scripts/package.sh
+++ scripts/package.sh
@@ -56,15 +56,16 @@ fi
 
 for BASE_IMAGE in ${BASE_IMAGES}; do
 
-    DIST_NAME=$(docker run  -v /tmp:/tmp -w ${TARGET_DIR} $BASE_IMAGE /bin/bash -c "apt-get update &&\
+    docker run  -v /tmp:/tmp -w ${TARGET_DIR} $BASE_IMAGE /bin/bash -c "apt-get update &&\
                                                                      apt-get -y upgrade &&\
                                                                      apt-get install -y ${TOOL_PACKAGES} &&\
                                                                      $EXTRA_CMD  &&\
                                                                      mk-build-deps -r -i -t \"apt-get -y \" &&\
   	                       		        	             dpkg-checkbuilddeps &&\
-                                                                     dpkg-buildpackage -us -uc -rfakeroot &&\
-                                                                     /usr/bin/lsb_release -s -c")
+                                                                     dpkg-buildpackage -us -uc -rfakeroot"
 
+    DIST_NAME=$(echo $BASE_IMAGE | cut -d':' -f 2)
+    
     dropbox_uploader.sh upload /tmp/librespot_${PACKAGE_VERSION}*.deb /Public/librespot/${DIST_NAME}/${ARCH}/
 
     rm -f /tmp/librespot*.deb
